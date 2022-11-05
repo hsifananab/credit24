fc26c358162e9850cdbce1eb9edf6e3ebd5624c7
Ian Iusov <yusov.yan@icloud.com>
2022-11-05 15:03:43 +0600

    feat: add sliders

--- src/components/Partners/PartnersSlider.tsx
+++ src/components/Partners/PartnersSlider.tsx
@@ -1,12 +1,55 @@
-import React from 'react';
+import Slider from 'react-slick';
+import LeftArrow from '../../assets/images/leftArrow.svg';
+import RightArrow from '../../assets/images/rightArrow.svg';
 import { partners } from '../../data/data';
 
-const PartnersSlider = () => {
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+export const PartnersSlider = () => {
+  const PartnersSliderArrowLeft = ({
+    currentSlide,
+    slideCount,
+    ...props
+  }: any) => (
+    <div className="" {...props}>
+      <div className="w-[50px] aspect-square bg-[#EDEDED] hover:bg-gradient-bg transition-all rounded-full flex items-center justify-center absolute top-1/2 -translate-y-1/2 -left-[50px]">
+        <img src={LeftArrow} alt="" />
+      </div>
+    </div>
+  );
+
+  const PartnersSliderArrowRight = ({
+    currentSlide,
+    slideCount,
+    ...props
+  }: any) => (
+    <div className="" {...props}>
+      <div className="w-[50px] aspect-square bg-[#EDEDED] hover:bg-gradient-bg transition-all rounded-full flex items-center justify-center absolute top-1/2 -translate-y-1/2 -right-[50px]">
+        <img src={RightArrow} alt="" />
+      </div>
+    </div>
+  );
+
+  const settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 6,
+    slidesToScroll: 2,
+    // autoplay: true,
+    // autoplaySpeed: 2000,
+    cssEase: 'linear',
+    prevArrow: <PartnersSliderArrowLeft />,
+    nextArrow: <PartnersSliderArrowRight />,
+  };
+
   return (
-    <div className="flex justify-between items-center">
-      {partners.map(item => (
-        <PartnersSliderItem key={item} item={item} />
-      ))}
+    <div>
+      <Slider {...settings}>
+        {partners.map(item => (
+          <PartnersSliderItem key={item} item={item} />
+        ))}
+      </Slider>
     </div>
   );
 };
@@ -20,7 +63,7 @@ interface PartnersSliderItemProps {
 const PartnersSliderItem = ({ item }: PartnersSliderItemProps) => {
   return (
     <div className="flex items-center justify-center w-[175px] h-[70px] bg-[#ECE4C0] rounded-[35px]">
-      <img src={item} alt="" />
+      <img src={item} alt="" className="" />
     </div>
   );
 };
--- src/components/Reviews/ReviewsSlider.tsx
+++ src/components/Reviews/ReviewsSlider.tsx
@@ -1,13 +1,51 @@
-import React from 'react';
+import Slider from 'react-slick';
+import LeftArrow from '../../assets/images/leftArrow.svg';
+import RightArrow from '../../assets/images/rightArrow.svg';
 import { reviewCards } from '../../data/data';
 
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
 const ReviewsSlider = () => {
+  const ReviewsSliderArrowLeft = ({
+    currentSlide,
+    slideCount,
+    ...props
+  }: any) => (
+    <div className="" {...props}>
+      <div className="w-[80px] aspect-square bg-white border border-[##DEDEDE] shadow-[0px_2px_7px_rgba(0,0,0,0.12)] transition-all rounded-full flex items-center justify-center absolute top-1/2 -translate-y-1/2 -left-[60px]">
+        <img src={LeftArrow} alt="" />
+      </div>
+    </div>
+  );
+
+  const ReviewsSliderArrowRight = ({
+    currentSlide,
+    slideCount,
+    ...props
+  }: any) => (
+    <div className="" {...props}>
+      <div className="w-[80px] aspect-square bg-white border border-[##DEDEDE] shadow-[0px_2px_7px_rgba(0,0,0,0.12)] transition-all rounded-full flex items-center justify-center absolute top-1/2 -translate-y-1/2 -right-[60px]">
+        <img src={RightArrow} alt="" />
+      </div>
+    </div>
+  );
+
+  const settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    prevArrow: <ReviewsSliderArrowLeft />,
+    nextArrow: <ReviewsSliderArrowRight />,
+  };
+
   return (
-    <div className="grid grid-cols-3 gap-[30px]">
+    <Slider {...settings} className="">
       {reviewCards.map(item => (
         <ReviewsSliderItem key={item.text} item={item} />
       ))}
-    </div>
+    </Slider>
   );
 };
 
@@ -25,7 +63,7 @@ interface ReviewsSliderItemProps {
 
 const ReviewsSliderItem = ({ item }: ReviewsSliderItemProps) => {
   return (
-    <div className="w-[380px] h-[323px] shadow-[0px_2px_7px_rgba(0,0,0,0.12)] rounded-[25px] pt-[28px] px-[12px] pb-[15px] flex flex-col gap-[15px]">
+    <div className="w-[380px] h-[323px] shadow-[0px_2px_7px_rgba(0,0,0,0.12)] rounded-[25px] my-1 mx-auto pt-[28px] px-[12px] pb-[15px] flex flex-col gap-[15px]">
       <div className="flex gap-[26px]">
         <img src={item.image} alt="" className="w-[85px] h-[75px]" />
         <div className="">
